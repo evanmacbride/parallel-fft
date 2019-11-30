10998d5c83e5d6bf2c2cb1b046b91142aba845b5
evanmacbride <ejmacbride@gmail.com>
2019-11-29 21:54:55 -0500

    declare j, k, size, halfsize, and tablestep within acc loop

--- src/acc-fft.cpp
+++ src/acc-fft.cpp
@@ -60,31 +60,31 @@ void Fft::transformRadix2(vector<complex<double> > &vec) {
 		if (j > i)
 			std::swap(vec[i], vec[j]);
 	}
-	size_t halfsize;
-	size_t tablestep;
-	complex<double> temp;
-	size_t size, j, k;
+	//size_t halfsize;
+	//size_t tablestep;
+	//complex<double> temp;
+	//size_t size, j, k;
 	// Cooley-Tukey decimation-in-time radix-2 FFT
 
-  #pragma acc data copy(vec, expTable) create(n)
-  {
-	for (size = 2; size <= n; size *= 2) {
-		halfsize = size / 2;
-		tablestep = n / size;
+#pragma acc data copy(vec, expTable)
+{
+	for (size_t size = 2; size <= n; size *= 2) {
+		size_t halfsize = size / 2;
+		size_t tablestep = n / size;
 
     #pragma acc parallel loop
 		for (i = 0; i < n; i += size) {
       #pragma acc loop
-			for (j = i, k = 0; j < i + halfsize; j++, k += tablestep) {
-				temp = vec[j + halfsize] * expTable[k];
+			for (size_t j = i, k = 0; j < i + halfsize; j++, k += tablestep) {
+				complex<double> temp = vec[j + halfsize] * expTable[k];
 				vec[j + halfsize] = vec[j] - temp;
 				vec[j] += temp;
 			}
 		}
-		if (size == n)  // Prevent overflow in 'size *= 2'
-			break;
+		//if (size == n)  // Prevent overflow in 'size *= 2'
+		//	break;
 	}
-  }
+}
 }
 
 static size_t reverseBits(size_t x, int n) {
