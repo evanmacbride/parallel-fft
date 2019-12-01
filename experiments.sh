9dda6f47186c31d5c41246f846d7646ccfe07b24
evanmacbride <ejmacbride@gmail.com>
2019-11-30 21:05:22 -0500

    run experiments.sh five times, each with separate output files

--- experiments.sh
+++ experiments.sh
@@ -4,44 +4,47 @@ echo "Compiling..."
 
 make all
 
-echo "Begin serial experiment"
+for (( i=0; i<5; i++ ))
+do
+  echo "Begin serial experiment $i"
 
-./serial-fft -noisy 0.05 440 > serial-results.txt
-./serial-fft -noisy 0.5 440 >> serial-results.txt
-./serial-fft -noisy 5 440 >> serial-results.txt
-./serial-fft -noisy 50 440 >> serial-results.txt
-./serial-fft -noisy 500 440 >> serial-results.txt
+  ./serial-fft -noisy 0.05 440 > serial-results$i.txt
+  ./serial-fft -noisy 0.5 440 >> serial-results$i.txt
+  ./serial-fft -noisy 5 440 >> serial-results$i.txt
+  ./serial-fft -noisy 50 440 >> serial-results$i.txt
+  ./serial-fft -noisy 500 440 >> serial-results$i.txt
 
-echo "Begin OpenMP experiment"
+  echo "Begin OpenMP experiment $i"
 
-srun -c 4 ./omp-fft -noisy 0.05 440 > omp-4cores-results.txt
-srun -c 4 ./omp-fft -noisy 0.5 440 >> omp-4cores-results.txt
-srun -c 4 ./omp-fft -noisy 5 440 >> omp-4cores-results.txt
-srun -c 4 ./omp-fft -noisy 50 440 >> omp-4cores-results.txt
-srun -c 4 ./omp-fft -noisy 500 440 >> omp-4cores-results.txt
+  srun -c 2 ./omp-fft -noisy 0.05 440 > omp-2cores-results$i.txt
+  srun -c 2 ./omp-fft -noisy 0.5 440 >> omp-2cores-results$i.txt
+  srun -c 2 ./omp-fft -noisy 5 440 >> omp-2cores-results$i.txt
+  srun -c 2 ./omp-fft -noisy 50 440 >> omp-2cores-results$i.txt
+  srun -c 2 ./omp-fft -noisy 500 440 >> omp-2cores-results$i.txt
 
-srun -c 8 ./omp-fft -noisy 0.05 440 > omp-8cores-results.txt
-srun -c 8 ./omp-fft -noisy 0.5 440 >> omp-8cores-results.txt
-srun -c 8 ./omp-fft -noisy 5 440 >> omp-8cores-results.txt
-srun -c 8 ./omp-fft -noisy 50 440 >> omp-8cores-results.txt
-srun -c 8 ./omp-fft -noisy 500 440 >> omp-8cores-results.txt
+  srun -c 4 ./omp-fft -noisy 0.05 440 > omp-4cores-results$i.txt
+  srun -c 4 ./omp-fft -noisy 0.5 440 >> omp-4cores-results$i.txt
+  srun -c 4 ./omp-fft -noisy 5 440 >> omp-4cores-results$i.txt
+  srun -c 4 ./omp-fft -noisy 50 440 >> omp-4cores-results$i.txt
+  srun -c 4 ./omp-fft -noisy 500 440 >> omp-4cores-results$i.txt
 
-srun -c 12 ./omp-fft -noisy 0.05 440 > omp-12cores-results.txt
-srun -c 12 ./omp-fft -noisy 0.5 440 >> omp-12cores-results.txt
-srun -c 12 ./omp-fft -noisy 5 440 >> omp-12cores-results.txt
-srun -c 12 ./omp-fft -noisy 50 440 >> omp-12cores-results.txt
-srun -c 12 ./omp-fft -noisy 500 440 >> omp-12cores-results.txt
+  srun -c 8 ./omp-fft -noisy 0.05 440 > omp-8cores-results$i.txt
+  srun -c 8 ./omp-fft -noisy 0.5 440 >> omp-8cores-results$i.txt
+  srun -c 8 ./omp-fft -noisy 5 440 >> omp-8cores-results$i.txt
+  srun -c 8 ./omp-fft -noisy 50 440 >> omp-8cores-results$i.txt
+  srun -c 8 ./omp-fft -noisy 500 440 >> omp-8cores-results$i.txt
 
-srun -c 16 ./omp-fft -noisy 0.05 440 > omp-16cores-results.txt
-srun -c 16 ./omp-fft -noisy 0.5 440 >> omp-16cores-results.txt
-srun -c 16 ./omp-fft -noisy 5 440 >> omp-16cores-results.txt
-srun -c 16 ./omp-fft -noisy 50 440 >> omp-16cores-results.txt
-srun -c 16 ./omp-fft -noisy 500 440 >> omp-16cores-results.txt
+  srun -c 16 ./omp-fft -noisy 0.05 440 > omp-16cores-results$i.txt
+  srun -c 16 ./omp-fft -noisy 0.5 440 >> omp-16cores-results$i.txt
+  srun -c 16 ./omp-fft -noisy 5 440 >> omp-16cores-results$i.txt
+  srun -c 16 ./omp-fft -noisy 50 440 >> omp-16cores-results$i.txt
+  srun -c 16 ./omp-fft -noisy 500 440 >> omp-16cores-results$i.txt
 
-echo "Begin OpenACC experiment"
+  echo "Begin OpenACC experiment $i"
 
-srun -p cisc372 --gres=gpu:1 ./acc-fft -noisy 0.05 440 > acc-results.txt
-srun -p cisc372 --gres=gpu:1 ./acc-fft -noisy 0.5 440 >> acc-results.txt
-srun -p cisc372 --gres=gpu:1 ./acc-fft -noisy 5 440 >> acc-results.txt
-srun -p cisc372 --gres=gpu:1 ./acc-fft -noisy 50 440 >> acc-results.txt
-srun -p cisc372 --gres=gpu:1 ./acc-fft -noisy 500 440 >> acc-results.txt
+  srun -p cisc372 --gres=gpu:1 ./acc-fft -noisy 0.05 440 > acc-results$i.txt
+  srun -p cisc372 --gres=gpu:1 ./acc-fft -noisy 0.5 440 >> acc-results$i.txt
+  srun -p cisc372 --gres=gpu:1 ./acc-fft -noisy 5 440 >> acc-results$i.txt
+  srun -p cisc372 --gres=gpu:1 ./acc-fft -noisy 50 440 >> acc-results$i.txt
+  srun -p cisc372 --gres=gpu:1 ./acc-fft -noisy 500 440 >> acc-results$i.txt
+done
